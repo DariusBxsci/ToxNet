fc0caa62a87ee1dda47fc1b41f3b678aefcd306c
Axtya Darius <barbanod@bxscience.edu>
2018-09-13 21:13:03 -0400

    complete mln working

--- Area 51/examples/toxic/new_toxic_complete/generate_rules.py
+++ Area 51/examples/toxic/new_toxic_complete/generate_rules.py
@@ -4,7 +4,16 @@ import sys
 toxidromes = open(sys.argv[1],'r').read().split(":")[0].split("\n")
 attributes = open(sys.argv[1],'r').read().split(":")[1].split("\n")
 
+print ("//Toxidromes")
+print ("\n".join(toxidromes))
+
+print ("//Attributes")
+print ("\n".join(attributes))
+
+print ("//Formulas")
+
 for t in toxidromes:
 	for a in attributes:
 		if len(t) > 1 and len(a) > 1:
-			print (t + " => " + a)
+			print ("log(0.8) " + t + " => " + a)
+
