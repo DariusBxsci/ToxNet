778fbb2d417bf7bf2a392b6948bc9f875fc1c973
Axtya Darius <barbanod@bxscience.edu>
2018-09-16 10:28:46 -0400

    Reorganized repo

--- env.sh
+++
@@ -1,4 +0,0 @@
-export PATH=$PATH:/Users/mac389/Documents/GitHub/ProbCog/apps
-export PYTHONPATH=$PYTHONPATH:/Users/mac389/Documents/GitHub/ProbCog/src/main/python
-export JYTHONPATH=$JYTHONPATH:/Users/mac389/Documents/GitHub/ProbCog/src/main/jython:/Users/mac389/Documents/GitHub/ProbCog/src/main/python
-export PROBCOG_HOME=/Users/mac389/Documents/GitHub/ProbCog
