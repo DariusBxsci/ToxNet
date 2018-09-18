0f7407f5370649836183464e713f592503c6dad1
Michael Chary <mac389@gmail.com>
2018-09-18 15:05:33 -0400

    Expanded world data generation

--- make_world.py
+++ make_world.py
@@ -1,3 +1,19 @@
 #make world
 
 from datagen import *
+#Entities are people, symptoms, perhaps toxidromes
+world = World()
+
+LIST_OF_ALL_POSSIBLE_SYMPTOMS = []
+
+class Person(Object):
+	def (self,world,intended_toxidrome,kwargs**):
+	Object.(self,"person")
+	
+	self.intended_toxidrome = intended_toxidrome
+
+	for key,value in kwargs.items():
+		setattr(self,key,value)
+
+class Toxidrome(Object):
+	def (self,)
