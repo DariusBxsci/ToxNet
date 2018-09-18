10c4d0fc5f99cda3aec668bdf336e400b683bae7
Michael Chary <mac389@gmail.com>
2018-09-18 13:43:41 -0400

    Use local paths

--- scripting.py
+++ scripting.py
@@ -1,11 +1,17 @@
 #run everything in python 
 #instead of minLearnShort.sh BASH script
 
+#must run source env.sh first
+
+import os 
+
 from MLN import *
 
 
 #Hard coding is bad; should log from cofig file
-mln = MLN("wts.pypll.toxic_complete-toxic_complete.mln")
-mrf = mln.groundMRF("toxic_complete.db")
+
+DB_path = os.path.join(".","ToxMLN","toxic_DB")
+mln = MLN(os.path.join(DB_path,"wts.pypll.toxic_complete-toxic_complete.mln"))
+mrf = mln.groundMRF(os.path.join(DB_path,"toxic_complete.db"))
 
 queries = []
