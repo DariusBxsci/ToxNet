e01a5272d6e44dc23ce61118fec89838e94e94aa
Michael Chary <mac389@gmail.com>
2018-09-18 13:38:25 -0400

    Adding to file

--- scripting.py
+++ scripting.py
@@ -2,3 +2,10 @@
 #instead of minLearnShort.sh BASH script
 
 from MLN import *
+
+
+#Hard coding is bad; should log from cofig file
+mln = MLN("wts.pypll.toxic_complete-toxic_complete.mln")
+mrf = mln.groundMRF("toxic_complete.db")
+
+queries = []
