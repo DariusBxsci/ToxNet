9ba142fad523a48eec4e14559d2c23f67eda87d1
Michael Chary <mac389@gmail.com>
2018-09-23 15:25:15 -0400

    Update training database to include label

--- src/make-train-db.py
+++ src/make-train-db.py
@@ -1,10 +1,10 @@
 import json 
 
-db = json.load(open('./world.json','rb'))
+db = json.load(open('./data/world.json','rb'))
 
 def convert_to_MLN(aDict):
-	return "//%s\n"% aDict['name'] + '\n'.join(['log(%.02f) %s(%s)'%(odds,symptom,aDict["name"]) 
-									for symptom, odds in aDict['presentation'].items()])
+	return "//%s\n"% aDict['name'] + '%s(%s)\n'%(aDict['intended_toxidrome'],aDict['name'])+'\n'.join(['log(%.02f) %s(%s)'%(odds,symptom,aDict["name"]) 
+									for symptom, odds in aDict['presentation'].items()]) + '\n'
 
-with open('world-train.db','wb') as outfile:
+with open('./data/world-train.db','wb') as outfile:
 	print>>outfile,'\n'.join([convert_to_MLN(info) for id,info in db.iteritems()])
