ce3bd5ffc4b5aaead761167afaa4a6da67e15256
Michael Chary <mac389@gmail.com>
2018-09-23 09:04:37 -0400

    complete list of toxidromes

--- make_world.py
+++ make_world.py
@@ -1,23 +1,53 @@
 #make world
+#trying to not use genDB, nothing works even after installing Jython
+import random
 
-from datagen import *
-#Entities are people, symptoms, perhaps toxidromes
-world = World()
+from pprint import pprint 
 
-LIST_OF_ALL_POSSIBLE_SYMPTOMS = []
+person = {"type":"patient",
+		   "name":"",
+		   "intended_toxidrome":"",
+		   "presentation":[]}
 
-class Person(Object):
-	def (self,world,intended_toxidrome,kwargs**):
-	Object.(self,"person")
-	
-	self.intended_toxidrome = intended_toxidrome
-
-	for key,value in kwargs.items():
-		setattr(self,key,value)
+nPatients = 2
 
 
-class Sign(Object):
-	
+toxidromes = {'sympathomimetic':{"tachycardic": 0.6,
+								"hypertensive":0.6,
+								"mydriatic": 0.6,
+								"diaphoretic": 0.6,
+								"decreased_bowel_sounds":0.6,
+								"tachypneic":0.6},
+				'sedative_hypnotic':{"eucardic":0.6,
+									"normotensive":0.6,
+									"normal_size_pupils":0.6,
+									"diaphoretic":0.6,
+									"decreased_bowel_sounds":0.6,
+									"bradypneic":0.6},
+				'cholinergic':{"bradycardic":0.6,
+								"hypotensive":0.6,
+								"miotic":0.6,
+								"diaphoretic":0.6,
+								"increased_bowel_sounds":0.6},
+				'anticholinergic': {"tachycardic":0.6,
+									"hypertensive":0.6,
+									"mydriatic":0.6,
+									"dry_skin":0.6,
+									"decreased_bowel_sounds":0.6},
+				'opioid':{"eucardic":0.6,
+							"normotensive":0.6,
+							"miotic":0.6,
+							"diaphoretic":0.6,
+							"decreased_bowel_sounds":0.6,
+							"bradypneic":0.6}
+							}
 
-class Toxidrome(Object):
-	def (self,)
+
+db = {}
+
+for n in xrange(nPatients):
+	 db[n] = person 
+	 intended_toxidrome = random.choice(toxidromes.keys())
+	 db[n]["intended_toxidrome"] = intended_toxidrome
+
+pprint(db)
--- test_for_world_gen.py
+++
@@ -1,7 +0,0 @@
-class Sign(object):
-	def __init__()
-
-"""
-	Tachycardic(person)
-	Eupneic(person)
-"""
